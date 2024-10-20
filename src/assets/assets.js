5754ebd4ea61288ff2d82e19af89baa7097df83d
abhi0561verma <abhishekverma0561@gmail.com>
2024-10-21 01:40:13 +0530

    Create Home Page

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -5,6 +5,7 @@ import profile_pic from './profile_pic.png'
 import contact_image from './contact_image.png'
 import about_image from './about_image.png'
 import logo from './logo.svg'
+import logoi from './lgo.jpg'
 import dropdown_icon from './dropdown_icon.svg'
 import menu_icon from './menu_icon.svg'
 import cross_icon from './cross_icon.png'
@@ -42,7 +43,7 @@ export const assets = {
     appointment_img,
     header_img,
     group_profiles,
-    logo,
+    logoi,
     chats_icon,
     verified_icon,
     info_icon,
